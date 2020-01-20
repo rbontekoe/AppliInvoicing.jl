4314ed73fdc5cb5292758c02da8ee023b1701b5d
Rob Bontekoe <rbontekoe@appligate.nl>
2020-01-20 09:45:46 +0100

    Update

--- src/api/api.jl
+++ src/api/api.jl
@@ -50,8 +50,8 @@ function conv2entry(inv::PaidInvoice, from::Int, to::Int)
     id = string(Date(now())) * "-" * string(global n += 1)
     customer_id = inv.header.name
     invoice_nbr = inv.header.invoice_nbr
-    debit = 0.0
-    credit = inv.stm.amount
+    debit = inv.stm.amount
+    credit = 0.0
     vat = 0.0
     descr = inv.body.name_training
     return create_journal_entry(id, customer_id, invoice_nbr, from, to, debit, credit, vat, descr)
