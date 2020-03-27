1e83d4b220067a99b020a6cbfdfee82d953cd96c
Rob Bontekoe <rbontekoe@appligate.nl>
2020-03-27 18:10:35 +0100

    Finish aging()

--- src/Reporting.jl
+++ src/Reporting.jl
@@ -9,13 +9,32 @@ using Dates
 
 using AppliInvoicing
 
+struct Aging
+    id_inv::String
+    csm::String
+    inv_date::Date
+    amount::Float64
+    days::Day
+end
+
 aging() = begin
     unpaid_invoices = AppliInvoicing.retrieve_unpaid_invoices(PATH_DB)
+    paid_invoices = AppliInvoicing.retrieve_paid_invoices(PATH_DB)
+
+    paid = [x.id for x in paid_invoices]
+    unpaid = filter(x -> x.id ∉ paid, unpaid_invoices)
+
     list = []
-    for invoice in unpaid_invoices
-        date = invoice.meta.date
-        println(date, " - ", Dates.today())
+    for invoice in unpaid
+        id = invoice.id
+        csm = invoice.header.name
+        inv_date = Date(invoice.meta.date)
+        amount = 1000.0
+        days = Dates.today() - inv_date
+        aging_item = Aging(id, csm, inv_date, amount, days)
+        push!(list, aging_item)
     end
+    return list
 end # aging
 
 end # module
