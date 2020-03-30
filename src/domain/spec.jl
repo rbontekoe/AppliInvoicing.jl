b86f1a0b19614128d15c216e4e0c9419170be028
Rob Bontekoe <rbontekoe@appligate.nl>
2020-03-30 08:23:13 +0200

    Update spec.jl

--- src/domain/spec.jl
+++ src/domain/spec.jl
@@ -10,58 +10,66 @@ abstract type BodyItem <: Structure end
 
 abstract type Payment <: Domain end
 
+#=
 """
     meta(i::Invoice)
 
 Returns the meta data from an invoice.
 """
 meta(i::Invoice) = error("Meta is not defined as concrete type")
+=#
 
 """
     meta(i::Invoice)
 
 Returns the meta data from an invoice.
 """
-function meta(i::Invoice) end
+function meta end
 
+#=
 """
     header(i::Invoice)
 
 Returns the meta data from an invoice.
 """
 header(i::Invoice) = error("Header is not defined as concrete type")
+=#
 
 """
     header(i::Invoice)
 
 Returns the meta data from an invoice.
 """
-function header(i::Invoice) end
+function header end
 
+#=
 """
     body(i::Invoice)
 
 Returns the meta data from an invoice.
 """
 body(i::Invoice) = error("Header is not defined as concrete type")
+=#
 
 """
     body(i::Invoice)
 
 Returns the meta data from an invoice.
 """
-function body(i::Invoice) end
+function body end
 
+#=
 """
     id(i::Invoice)
 
 Returns the id of an invoice.
 """
 id(i::Invoice) = error("Header is not defined as concrete type")
+=#
 
 """
     id(i::Invoice)
 
 Returns the id of an invoice.
 """
-function id(i::Invoice) end
+function id end
